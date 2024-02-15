ba0e397e65355e459051f6065748e27accfe696b
GawdOfGithub <anuragbhandari77736@gmail.com>
2024-02-15 01:04:36 -0500

    Saving progress on this branch: animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ interface HoverProps {
 
 const Hover = ({  }: HoverProps) => {
   return (
-    <div className="grid place-items-center h-full w-fu bg-blue-400 hover:bg-purple-400 group" >
+    <div className="grid place-items-center h-full w-full transition-colors duration-5000  bg-blue-400 hover:bg-purple-400 group ease-in-out delay-300" >
     <div className='h-5 w-5 bg-black group-hover:bg-red-600'></div>
       <div className='h-5 w-5 bg-black group-hover:bg-blue-600'></div>
     </div>
